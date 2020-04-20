2b9342387110b5f507362661002729c6216f2bf5
Jobson P Varghese <jobsonvarghese1@gmail.com>
2020-04-21 02:26:59 +0530

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -74,23 +74,23 @@
   });
 
   // Navigation active state on scroll
-  var nav_sections = $('section');
-  var main_nav = $('.main-nav, .mobile-nav');
-  var main_nav_height = $('#header').outerHeight();
+  // var nav_sections = $('section');
+  // var main_nav = $('.main-nav, .mobile-nav');
+  // var main_nav_height = $('#header').outerHeight();
 
-  $(window).on('scroll', function () {
-    var cur_pos = $(this).scrollTop();
+  // $(window).on('scroll', function () {
+  //   var cur_pos = $(this).scrollTop();
   
-    nav_sections.each(function() {
-      var top = $(this).offset().top - main_nav_height,
-          bottom = top + $(this).outerHeight();
+  //   nav_sections.each(function() {
+  //     var top = $(this).offset().top - main_nav_height,
+  //         bottom = top + $(this).outerHeight();
   
-      if (cur_pos >= top && cur_pos <= bottom) {
-        main_nav.find('li').removeClass('active');
-        main_nav.find('a[href="#'+$(this).attr('id')+'"]').parent('li').addClass('active');
-      }
-    });
-  });
+  //     if (cur_pos >= top && cur_pos <= bottom) {
+  //       main_nav.find('li').removeClass('active');
+  //       main_nav.find('a[href="#'+$(this).attr('id')+'"]').parent('li').addClass('active');
+  //     }
+  //   });
+  // });
 
   // jQuery counterUp (used in Whu Us section)
   $('[data-toggle="counter-up"]').counterUp({
